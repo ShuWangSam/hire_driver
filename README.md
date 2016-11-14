5594e14a9faf4e96c470460509c74f2ef15ede74
SamShuWang <shu.wang@hotmail.ca>
2016-11-14 17:34:16 -0500

    feat(hire_driver model):add hire_driver model

---
+++ README.md
@@ -0,0 +1,1 @@
+# cm_author_input
--- js/driverHiringController.js
+++ js/driverHiringController.js
@@ -1,6 +1,49 @@
-var driverHiring = angular.module(`driverHiring`, []);
+
+
+
+angular.module('ng').filter('tel', function () {
+    return function (tel) {
+    console.log(tel)
+        if (!tel) { return ''; }
+
+        var value = tel.toString().trim().replace(/^\+/, '');
+
+        //if (value.match(/[^0-9]/)) {
+          //  return tel;
+       // }
+        var city='';
+        var area='';
+        var number = '';
+        if(value.length >= 3){
+            city ="("+value.slice(0, 3)+")";
+           if(value.length <=6 && value.length >3 ){
+           area = value.slice(3,6);
+           }
+           if(value.length>=6){
+           		 area = value.slice(3,6)+"-";
+          	 console.log(area)
+
+           }
+           if(value.length>6){
+            number = value.slice(6,10);
+           }
+
+
+           const telp =city +area+number
+
+           return telp;
+
+         }
+            };
+});
+
+
+var driverHiring = angular.module(`driverHiring`, [])
 driverHiring.controller(`driverHiringController`, function($http){
   var dhc = this;
+  dhc.formatTel = function(){
+    dhc.applyDriver.tel = dhc.applyDriver.tel|"tel";
+  }
   dhc.statusOptions =[
       {
         "optionName":"工签",
@@ -45,11 +88,11 @@ driverHiring.controller(`driverHiringController`, function($http){
   ];
   dhc.areaOptions =[
       {
-        "optionName":"Scarborough /& Markham",
+        "optionName":"Scarborough & Markham",
         "selected":false
       },
       {
-        "optionName":"Richmond Hill /& North York",
+        "optionName":"Richmond Hill & North York",
         "selected":false
       },
       {
@@ -71,7 +114,7 @@ driverHiring.controller(`driverHiringController`, function($http){
         "selected":false
       },
       {
-        "optionName":"Peak Time: Weekdays: 12:00-14:00,18:00 - 21:00.\n  Weekends: 12:00-15:00, 17:00 - 21:00",
+        "optionName":"Peak Time: \n  Weekdays: 12:00-14:00,\n  18:00 - 21:00.\n  Weekends: 12:00-15:00,\n  17:00 - 21:00",
         "selected":false
       }
     ];
@@ -143,7 +186,15 @@ driverHiring.controller(`driverHiringController`, function($http){
         experience.selected = false;
     });
    }
+
   dhc.outPutDriver = function(){
+    if(!dhc.applyDriver.name || !dhc.applyDriver.status||
+       !dhc.applyDriver.tel||!dhc.applyDriver.email||!dhc.applyDriver.sin||
+        JSON.stringify(dhc.applyDriver.area)=="[]"||JSON.stringify(dhc.applyDriver.hour)=="[]"){
+
+    }else{
+
+   dhc.buttonDisabled = true;
    dhc.driverInformaiton =
     `<!DOCTYPE html>
     <html>
@@ -166,6 +217,7 @@ driverHiring.controller(`driverHiringController`, function($http){
     </html>`
     console.log(dhc.driverInformaiton)
 
+
     $http({
       method: 'POST',
       url: 'https://www.chanmao.ca/index.php?r=Site/ApplyDriver',
@@ -179,4 +231,42 @@ driverHiring.controller(`driverHiringController`, function($http){
       console.log(response)
       });
     }
+    }
 })
+driverHiring.directive('phoneInput', function($filter, $browser) {
+    return {
+        require: 'ngModel',
+        link: function($scope, $element, $attrs, ngModelCtrl) {
+            var listener = function() {
+                var value = $element.val().replace(/[^0-9]/g, '');
+                $element.val($filter('tel')(value, false));
+            };
+
+            // This runs when we update the text field
+            ngModelCtrl.$parsers.push(function(viewValue) {
+                return viewValue.replace(/[^0-9]/g, '').slice(0,10);
+            });
+
+            // This runs when the model gets updated on the scope directly and keeps our view in sync
+            ngModelCtrl.$render = function() {
+                $element.val($filter('tel')(ngModelCtrl.$viewValue, false));
+            };
+
+            $element.bind('change', listener);
+            $element.bind('keydown', function(event) {
+                var key = event.keyCode;
+                // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+                // This lets us support copy and paste too
+                if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)){
+                    return;
+                }
+                $browser.defer(listener); // Have to do this or changes don't get picked up properly
+            });
+
+            $element.bind('paste cut', function() {
+                $browser.defer(listener);
+            });
+        }
+
+    };
+});
